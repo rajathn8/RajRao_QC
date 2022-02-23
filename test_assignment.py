8675a7c0a326f17b93458338451e8877d9eeb809
Rajath Rao <rajathrao@Rajaths-MBP.Dlink>
2022-02-23 19:41:00 +0530

    Adding Tests

--- test_assignment.py
+++ test_assignment.py
@@ -1,15 +1,30 @@
 import unittest
 from list_approach import read_log_file, return_most_active_cookie
-from pandas_approach import read_log_file,parse_input_date,return_most_active_cookie
+
 
 class TestAssignment(unittest.TestCase):
-    
-    def test_pandas_date(self):
-        result = parse_input_date('2018-12-09')
-        self.assertEqual(1,1)
+
+    def test_read_file(self):
+        result = read_log_file('QC.csv', '2018-12-09')
+        self.assertIsNotNone(result)
+
+    def test_return_most_active_cookie_1(self):
+        '''
+        Input date is there in the log file
+        '''
+        result = return_most_active_cookie(
+            read_log_file('QC.csv', '2018-12-09'))
+        self.assertEqual(['AtY0laUfhglK3lC7'], result)
+
+    def test_return_most_active_cookie_2(self):
+        '''
+        Input date is not present in the Log File
+        '''
+        result = return_most_active_cookie(
+            read_log_file('QC.csv', '2018-12-22'))
+        self.assertEqual(['No Cookie for Mentioned Date'], result)
 
 
 if __name__ == '__main__':
-    
+
     unittest.main()
-        
