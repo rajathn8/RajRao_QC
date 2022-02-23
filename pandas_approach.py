6b70c7da18d302f394a52db0b72f4dcc9f50a379
Rajath Rao <rajathrao@Rajaths-MBP.Dlink>
2022-02-23 17:40:51 +0530

    pylint for pandas_approach

--- pandas_approach.py
+++ pandas_approach.py
@@ -6,49 +6,56 @@ import argparse
 import datetime as dt
 import pandas as pd
 
-file_name = 'QC.csv'
-date_from_cli = '2018-12-09'
 
 def read_log_file(file_name):
     '''Reading CSV File '''
 
-    log_file = pd.read_csv(file_name)
+    file = pd.read_csv(file_name)
+    '''
+    To Do 
+    --> Add Chunks
+    --> We can add additional checks to make sure our file is not corrupt
+    --> Exception Handling if file not found 
+    '''
 
-    # To Do --> Add Chunks 
+    return file
 
-    return log_file
 
 def parse_input_date(input_date):
     '''Parse the Input Date to datetime format'''
-    
+
     input_date = dt.datetime.strptime(input_date, '%Y-%m-%d').date()
-    
+
     return input_date
 
-def return_most_active_cookie(df,input_date):
-    
+
+def return_most_active_cookie(input_log_file, input_date):
     '''
     Function takes in a Dataframe and returns most active cookie for the specified date
     '''
     # Convert TimeStamp to datetime
     # New Column created with date
-    log_file['date'] = pd.to_datetime(log_file['timestamp']).dt.date
-    
-    filtererd_cookie_for_day = log_file[log_file['date']==input_date]['cookie']
-    return filtererd_cookie_for_day.mode()[0] if 0<len(filtererd_cookie_for_day) else 'No Cookie For Specified date'
+    input_log_file['date'] = pd.to_datetime(
+        input_log_file['timestamp']).dt.date
+
+    filtererd_cookie_for_day = input_log_file[input_log_file['date']
+                                              == input_date]['cookie']
+    return filtererd_cookie_for_day.mode()[0] if 0 < len(filtererd_cookie_for_day) else 'No Cookie For Specified date'
 
 
 if __name__ == '__main__':
-    
-    parser = argparse.ArgumentParser(description='Return the most active cookie')
-    parser.add_argument('-f','--filename',help = 'File to be Parsed')
-    parser.add_argument('-d','--date',help = 'mention the date to get its most active cookie')
+
+    parser = argparse.ArgumentParser(
+        description='Return the most active cookie')
+    parser.add_argument('-f', '--filename', help='File to be Parsed')
+    parser.add_argument(
+        '-d', '--date', help='mention the date to get its most active cookie')
     args = parser.parse_args()
-    
+
     log_file = read_log_file(args.filename)
-    input_date = parse_input_date(args.date)
-    cookie = return_most_active_cookie(log_file,input_date)
-    
+    cookie_date = parse_input_date(args.date)
+    cookie = return_most_active_cookie(log_file, cookie_date)
+
     print(cookie)
 
 '''
@@ -65,4 +72,7 @@ Test Cases
 5 -  Input Log file format is not correct
 6 -  Input Log file is big - Have to use chunks
 
+'''
+'''
+Each Operation has been breaken down into multiple operations to add more features
 '''
